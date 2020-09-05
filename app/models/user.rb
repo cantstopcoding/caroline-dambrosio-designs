07a8de43e04ec6996c689991a74a328703953dba
cantstopcoding <jonathan_koss@yahoo.com>
2020-09-05 12:27:30 -0400

    create associations in user model and add has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password
+    
+    has_many :posts
+    has_many :items
+    has_many :item_posts, through :items
+    has_many :categories, through :posts
 end
