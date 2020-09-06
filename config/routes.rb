90e3caca4c8d895ea7871f99941d92b2b27a6209
cantstopcoding <jonathan_koss@yahoo.com>
2020-09-05 20:27:26 -0400

    fix sessions route typo and add login and delete routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   # add custom routes on top
-  root 'session#home'
+  root 'sessions#home'
   
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+
+  delete '/logout' => 'sessions#destroy'
   
   resources :categories
   resources :items
