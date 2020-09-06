eb4f384a71d3cd801ba2b37d00b90e983d1576de
cantstopcoding <jonathan_koss@yahoo.com>
2020-09-06 15:56:17 -0400

    redirect to root path if user is falsey

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
     end
     
     def create
-        # binding.pry
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
@@ -15,7 +14,10 @@ class UsersController < ApplicationController
     end
 
     def show
+        # use find_by instead of find because find will throw up error 
+        # and find_by will return nil
         @user = User.find_by(id: params[:id])
+        redirect_to '/' if !@user
     end
 
     private
