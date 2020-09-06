df9c71f501ce2cfa1354cde2ee0e5250172ff02d
cantstopcoding <jonathan_koss@yahoo.com>
2020-09-06 19:41:02 -0400

    add conditional statement for user if they log in correctly or not

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,13 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:user][:email])
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user) #=> "/users/#{user.id}"
+    else
+      flash[:message] = "Incorrect info, please try agin"
+      redirect_to '/login'
+    end 
   end
 
   def destroy
