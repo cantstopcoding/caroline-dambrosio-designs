96d488e336c57d8b2aaa7a1c31a0f4b24899f4ba
cantstopcoding <jonathan_koss@yahoo.com>
2020-09-07 16:54:46 -0400

    add index and new actions, and a strong params

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,15 @@
 class ItemsController < ApplicationController
+    before_action :redirect_if_not_logged_in
+    
+    def index
+    end
+    
+    def new
+    end
+
+    private
+
+  def item_params
+    params.require(:item).permit(:name, :image, :price, :description, :post_id)
+  end
 end
