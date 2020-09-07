de01589cf87897127bb618e5cfaad5c46151bf02
cantstopcoding <jonathan_koss@yahoo.com>
2020-09-07 14:57:02 -0400

    create a strong params and a create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,20 @@ class PostsController < ApplicationController
     def new
         @post = Post.new 
     end
+
+    def create
+        binding.pry
+        @post = current_user.posts.build(post_params)
+        if @post.save
+            redirect_to posts_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:title, :content)
+      end
 end
