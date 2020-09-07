0083eff7d380fd691a05d3982f6a4417779e0c30
cantstopcoding <jonathan_koss@yahoo.com>
2020-09-06 20:09:22 -0400

    add current_user, logged_in? and redirect_to_if_not_logged_in method to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+    # I used the name @current_user so nothing get overridden
+    # this is called memoization 
+    def current_user
+        @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id] #=> use if statement so as to not make a call to database, if not necessary
+    end
+
+    def logged_in?
+        !!session[:user_id] #=> more secure way is if @current_user is truthy
+    end
+
+    def redirect_if_not_logged_in
+        # maybe display flash message
+        redirect_to '/' if !logged_in?
+    end
 end
