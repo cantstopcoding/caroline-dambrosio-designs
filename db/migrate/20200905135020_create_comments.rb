e7d32e926104f170029c6f97a26701b109f3143d
cantstopcoding <jonathan_koss@yahoo.com>
2020-09-08 11:51:59 -0400

    put in correct attributes for comments table

--- db/migrate/20200905135020_create_comments.rb
+++ db/migrate/20200905135020_create_comments.rb
@@ -1,10 +1,7 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.string :name
-      t.string :image
-      t.float :price
-      t.text :description
+      t.string :content
       t.belongs_to :user, null: false, foreign_key: true
       t.belongs_to :item, null: false, foreign_key: true
 
