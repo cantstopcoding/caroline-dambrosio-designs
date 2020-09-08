b8f66e248953f67f6fc7e4b7486b6f1e320ea482
cantstopcoding <jonathan_koss@yahoo.com>
2020-09-07 22:37:28 -0400

    add create, show, edit and update actons

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,36 @@ class ItemsController < ApplicationController
     before_action :redirect_if_not_logged_in
     
     def index
+      @items = Item.all 
     end
     
     def new
+      @item = Item.new 
+    end
+
+    def create 
+      @item = current_user.items.build(item_params)
+        if @item.save
+            redirect_to items_path
+        else
+            render :new
+        end
+    end
+    
+    def show 
+      @item = Item.find_by(id: params[:id])
+    end
+
+    def edit
+      @item = Item.find_by(id: params[:id])
+    end
+
+    def update
+      if @item.update(item_params)
+        redirect_to item_path(@item)
+      else
+        render :edit
+      end
     end
 
     private
