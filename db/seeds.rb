4c1d62369651ee588baf9ca91d5561b336ffa984
cantstopcoding <jonathan_koss@yahoo.com>
2020-09-07 22:39:09 -0400

    create seed data for post and item models

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,7 @@
 
 User.create([{first_name: "Caroline", last_name: "D'ambrosio", username: "bestdesigner", email: "caroline@caroline.com", password: "pw"}, {first_name: "Coco", last_name: "Chanel", username: "chanel", email: "chanel@chanel.com", password: "pw"}, {first_name: "Christian", last_name: "Dior", username: "dior", email: "dior@dior.com", password: "pw"}])
 
+Post.create([{title: "Outlander Inspired Costume", content: "I love Outlander costumes so I made one that looks like it!", user_id: 1}, {title: "A Purse You Can't Afford", content: "You can't afford this don't even bother", user_id: 2}, {title: "Super Expensive Sandals", content: "It's not that comfortable", user_id: 3}])
+
+# Make price float later on
+Item.create([{name: "Outlander Costume", image: "https://yt3.ggpht.com/a/AATXAJx5mEunDOUJudzpBNqD61pppw8kcQvgmDHMosko=s100-c-k-c0xffffffff-no-rj-mo", price: 500, description:"It's awesome!", user_id: 1, post_id: 1}, {name: "Expensive Purse", image: "https://cdn.shopify.com/s/files/1/0384/0161/products/56265-92_20Chanel_20Vintage_20Classic_20Double_20Flap_20Bag_20Qu_2D_0002_518x518.jpg?v=1591021463", price: 1000000000000, description:"It's awesome!", user_id: 2, post_id: 2}, {name: "Expensive Sandals", image: "//images.neimanmarcus.com/ca/5/product_assets/X/4/E/E/1/NMX4EE1_mu.jpg", price: 5000000000000, description: "It's too much damn money", user_id: 2, post_id: 2}])
