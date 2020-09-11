11b466d2b9124f202605d32558064d3852870848
cantstopcoding <jonathan_koss@yahoo.com>
2020-09-11 11:55:24 -0400

    get id of shallow routing for the nested comment resource comment of items

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
     resources :items, only: [:new, :create, :index] #=> only: [:new, :create, :index] is called shallow routing 
   end
   resources :items do 
-    resources :comments , only: [:new, :create, :index]
+    resources :comments
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
