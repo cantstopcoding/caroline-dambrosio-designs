c5c59fafa848d39ab40515d2d435b0904e842ce4
cantstopcoding <jonathan_koss@yahoo.com>
2020-09-12 13:48:33 -0400

    add omnilogin action and auth method in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,4 +20,25 @@ class SessionsController < ApplicationController
     session.clear
     redirect_to root_path
   end
+
+  def omnilogin
+    @user = User.find_or_create_by(email: auth[:info][:email]) do |user|
+      user.first_name = auth[:info][:first_name]
+      user.last_name = auth[:info][:last_name]
+      user.username = auth[:info][:first_name]
+      user.password = SecureRandom.hex
+    end
+    if @user.save
+      session[:user_id] = @user.id 
+      redirect_to user_path(@user)
+    else
+      redirect_to '/'
+    end
+  end 
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
+  end
 end
