2e31de4bfae7c121219b06921921dfbc8a62165b
cantstopcoding <jonathan_koss@yahoo.com>
2020-09-14 11:00:47 -0400

    in items controller, replace alpha scope method with most_comments method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,10 +16,10 @@ class ItemsController < ApplicationController
 
     def index 
         if params[:user_id] && @user = User.find_by(id: params[:user_id])
-            @items = @user.items.alpha 
+            @items = @user.items.most_comments  
         else
             @error = "User doesn't exist" if params[:user_id] # user @error because flash persists through 1 redirect
-            @items = Item.alpha
+            @items = Item.most_comments 
         end
     end
 
