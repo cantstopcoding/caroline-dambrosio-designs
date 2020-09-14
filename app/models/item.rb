f82d6f21c746e4cd0d3d288d5425de0393d56066
cantstopcoding <jonathan_koss@yahoo.com>
2020-09-14 10:56:27 -0400

    in item scope add most_comments scope method that orders comments by the most comments they have

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,8 @@ class Item < ApplicationRecord
   # belongs_to :category
 
   scope :alpha, -> { (order(:name)) }
+  scope :most_comments, -> { left_joins(:comments).group('items.id').order('count(comments.item_id) desc') } 
+  # :comments is AREL syntax 'items.id' is query syntax and it's using plural table (items) name and column name (id)
 end
 
 
