5903ab5b256f4b761abf4dae9b7a1f8e216aafe2
cantstopcoding <jonathan_koss@yahoo.com>
2020-09-15 18:14:47 -0400

    in comments ontrollerr create helper methods to keep it more dry

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
     before_action :redirect_if_not_logged_in
+    before_action :set_comment, only: [:show, :edit, :update]
+    before_action :redirect_if_not_comment_author, only: [:edit, :update]
     
     def index
       if params[:item_id] && @item = Item.find_by(id: params[:item_id])
@@ -29,14 +31,13 @@ class CommentsController < ApplicationController
     end
     
     def show 
-      @comment = Comment.find_by(id: params[:id])
     end
 
     def edit
-      @comment = Comment.find_by(id: params[:id])
     end
 
     def update
+      @comment = Comment.find_by(id: params[:id])
       if @comment.update(comment_params)
         redirect_to comment_path(@comment)
       else
@@ -48,5 +49,17 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:content, :item_id)
+  end
+
+  def set_comment
+    @comment = Comment.find_by(id: params[:id])
+    if !@comment
+      flash[:message] = "Comment was not found"
+      redirect_to comments_path
+    end
+  end
+
+  def redirect_if_not_comment_author
+    redirect_to comments_path if @comment.user != current_user
   end
 end
