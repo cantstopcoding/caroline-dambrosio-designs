99939eac4a68008e0be336366ca3ae3266c88329
cantstopcoding <jonathan_koss@yahoo.com>
2020-09-15 11:43:17 -0400

    in item model, add custom vaalidator, to_many_items

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,12 +5,23 @@ class Item < ApplicationRecord
   # why has many users though?
   # belongs_to :category
 
-  validates :name, :image, :price, :description, :content, presence: true
+  validates :name, :image, :price, :description, presence: true
   # form for field creates a fild_with_errors and the page must be rendered, in addition with presence: true
+  validate :too_many_items
 
   scope :alpha, -> { (order(:name)) }
   scope :most_comments, -> { left_joins(:comments).group('items.id').order('count(comments.item_id) desc') } 
   # :comments is AREL syntax 'items.id' is query syntax and it's using plural table (items) name and column name (id)
+
+  def too_many_items
+    todays_items = user.items.select do |item|
+      item.created_at.try(:to_date) == Date.today
+    end
+    if todays_items.size > 19
+      errors.add(:item_id, "can't post more than 20 times a day.")
+    end
+    # errors.add()
+  end 
 end
 
 
