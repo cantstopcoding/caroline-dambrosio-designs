c304796569a07706fb6367fdec6cbe8fbe95fe3b
cantstopcoding <jonathan_koss@yahoo.com>
2020-09-18 22:58:20 -0400

    in comments controller create display_edit_or_delete method

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,8 @@
 module CommentsHelper
+    def display_edit_or_delete
+        if @comment.user == current_user
+            link_to("Update Comment", edit_comment_path(@comment)) +
+            button_to("Delete Comment", comment_path(@comment), :method => "delete")
+        end
+    end
 end
