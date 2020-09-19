68504cce4aaad0141a16ba76b1d03f1794b55d3f
cantstopcoding <jonathan_koss@yahoo.com>
2020-09-18 23:00:45 -0400

    change to display_edit_and_delete from display_edit_or_delete

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,5 +1,5 @@
 module CommentsHelper
-    def display_edit_or_delete
+    def display_edit_and_delete
         if @comment.user == current_user
             link_to("Update Comment", edit_comment_path(@comment)) +
             button_to("Delete Comment", comment_path(@comment), :method => "delete")
