a56460115633bdca036460b208ade8cadb6dc1e4
cantstopcoding <jonathan_koss@yahoo.com>
2020-09-20 14:39:32 -0400

    comment out delegat name and add create class search method

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :user
+  belongs_to :category
   has_many :comments, dependent: :destroy
   has_many :users, through: :comments
   # why has many users though?
-  belongs_to :category
 
-  delegate :name, to: :category
+  # delegate :name, to: :category
 
   validates :name, :image, :price, :description, presence: true
   # form for field creates a fild_with_errors and the page must be rendered, in addition with presence: true
@@ -15,6 +15,10 @@ class Item < ApplicationRecord
   scope :most_comments, -> { left_joins(:comments).group('items.id').order('count(comments.item_id) desc') } 
   # :comments is AREL syntax 'items.id' is query syntax and it's using plural table (items) name and column name (id)
 
+  def self.search(params)
+    left_joins(:comments).where("LOWER(items.name) LIKE :term OR LOWER(items.description) LIKE :term OR LOWER(comments.content) LIKE :term", term: "%#{params}%")
+  end
+  
   def self.filter(params)
     where("category_id = ?", params)
   end
