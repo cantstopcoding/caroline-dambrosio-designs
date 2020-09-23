51958c63996136218983b6d35c2b58edc9ef38db
cantstopcoding <jonathan_koss@yahoo.com>
2020-09-23 19:09:01 -0400

    create and implement item count method

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ApplicationRecord
     has_many :items
-    # has_many :users, through: :items 
+    has_many :users, through: :items 
+
+    def items_count
+        items.count
+    end
 end
