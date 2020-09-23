a37377c1b84193547b027fb46da7cf0b4c864dd2
cantstopcoding <jonathan_koss@yahoo.com>
2020-09-23 18:21:25 -0400

    add image_url to strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:first_name, :last_name, :username, :email, :password)
+        params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :image_url)
     end
 
 end
