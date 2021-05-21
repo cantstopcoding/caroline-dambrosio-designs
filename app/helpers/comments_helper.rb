a7fae8598e6a06fef0b569d4fe82182ed18d282d
cantstopcoding <jonathan_koss@yahoo.com>
2021-05-21 13:40:25 -0400

    create hidden_field_or_display_collection_select helper

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -6,3 +6,11 @@ module CommentsHelper
     end
   end
 end
+
+def hidden_field_or_display_collection_select
+  if !@comment.item
+    select_tag "comment[item_id]", options_from_collection_for_select(Item.all, :id, :name)
+  else
+    hidden_field_tag "comment[item_id]", @comment.item_id
+  end
+end
